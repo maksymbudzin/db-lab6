1b451529d2ef3ebda2aceebffe7d960e670e75ef
maksymbudzin <55983798+maksymbudzin@users.noreply.github.com>
2021-05-30 16:55:56 +0300

    Update HealthCheckController.java

--- src/main/java/com/budzin/controller/HealthCheckController.java
+++ src/main/java/com/budzin/controller/HealthCheckController.java
@@ -1,21 +1,14 @@
 package com.budzin.controller;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/healthcheck")
 @RestController
 public class HealthCheckController {
-
-    @RequestMapping(method = RequestMethod.GET)
-
-    public ResponseEntity<?> getAll() {
-
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
-
+  
+  @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+  public ResponseEntity returnHealthCheckResponse() {
+      return new ResponseEntity<>("Everything is OK", HttpStatus.OK);
+  }
 }
